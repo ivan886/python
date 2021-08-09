2be3c9b33938b29061d68bf6b381fe88940d753c
ivan <ivanfernando.fonseca@gmail.com>
2021-08-09 14:52:58 -0500

    basic

---
+++ basic.py
@@ -0,0 +1,17 @@
+
+x = 2
+y = 2.5
+name = 'brad'
+x, y, name, is_cool=(1,2.5,'brad',True)
+
+a = x+y
+print(type(is_cool))   #  type indica el tipo de dato
+print(x,y,name,is_cool)
+
+#Casting
+x=str(x)
+y=int(y)
+
+print(y)
+
+print('hello '+'nombres')
