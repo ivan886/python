aea481060c854f7422c946d84d88b35ed9e0cc6c
ivan <ivanfernando.fonseca@gmail.com>
2021-08-09 17:02:37 -0500

    condicionales

---
+++ condicionales.py
@@ -0,0 +1,17 @@
+x=2
+y=7
+
+if  x>y:
+   print(f'{x} is greater to {y}')
+
+if x>1:
+  if x<=10:
+     print(f'{x}')
+
+
+key = "abc"
+password = input("Introduce la contraseña: ")
+if key == password.lower():
+    print("Puede ingresar al sistema")
+else:
+    print("La contraseña no coincide ")
