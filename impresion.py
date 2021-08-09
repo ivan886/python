2cbf600589cc5da230f83fe9f48029e7fadf75cd
ivan <ivanfernando.fonseca@gmail.com>
2021-08-09 15:09:53 -0500

    print

---
+++ impresion.py
@@ -0,0 +1,26 @@
+parrafo = ''' hola esto es 
+                 es un texto con 
+                 varias lineas
+                 '''
+
+print(parrafo)
+
+print('hola mundo')
+print(5 * 10)
+
+
+name = input('Registra tu nombre ')
+
+print(name)
+
+x = input('Registra el primer número  ')
+y = input('Registra el segundo número  ')
+
+
+z = int(x) * int(y)
+print(f'The result of { x } * { y } = { z }')
+
+name='brad'
+age=15
+
+print(f'my name is {name} and i am {age}')
